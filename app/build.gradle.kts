eb08d153fe2c8f1aa71e0b2bbaebf5af439746ff
Tohir Mirxomitov <mirxtohir@gmail.com>
2024-09-02 21:05:13 +0500

    UPD : v_name=1.3 v_code=6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "uz.gita.game2048v1"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.2"
+        versionCode = 6
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
